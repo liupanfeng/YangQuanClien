3d0b81387e1d25c76528feda9a37b3a0e3a9c8e0
liupanfeng <569307092@qq.com>
2020-12-20 13:04:55 +0800

    完善 羊管家 我的业务

--- app/src/main/java/com/meishe/yangquan/activity/SheepMyBusinessActivity.java
+++ app/src/main/java/com/meishe/yangquan/activity/SheepMyBusinessActivity.java
@@ -2,6 +2,8 @@ package com.meishe.yangquan.activity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.meishe.yangquan.R;
 import com.meishe.yangquan.utils.AppManager;
@@ -13,6 +15,26 @@ public class SheepMyBusinessActivity extends BaseActivity {
 
     private Button mBtnAply;
     private Button mBtnBack;
+    /**
+     * 没有服务申请信息
+     */
+    private LinearLayout mLlNoMessage;
+    /**
+     * 复审
+     */
+    private RelativeLayout mRlMyBusinessReview;
+    /**
+     * 申请
+     */
+    private RelativeLayout mRlMyBusinessApply;
+    /**
+     * 初审
+     */
+    private RelativeLayout mRlMyBusinessFirst;
+    /**
+     * 完成
+     */
+    private RelativeLayout mRlMyBusinessFinish;
 
     @Override
     protected int initRootView() {
@@ -25,11 +47,16 @@ public class SheepMyBusinessActivity extends BaseActivity {
         mIvBack = findViewById(R.id.iv_back);
         mBtnAply = findViewById(R.id.btn_apply);
         mBtnBack = findViewById(R.id.btn_back);
+        mLlNoMessage = findViewById(R.id.ll_no_message);
+        mRlMyBusinessReview = findViewById(R.id.rl_my_business_review);
+        mRlMyBusinessApply = findViewById(R.id.rl_my_business_apply);
+        mRlMyBusinessFirst = findViewById(R.id.rl_my_business_first);
+        mRlMyBusinessFinish = findViewById(R.id.rl_my_business_finish);
     }
 
     @Override
     public void initData() {
-
+        showNoMessage();
     }
 
     @Override
@@ -89,5 +116,59 @@ public class SheepMyBusinessActivity extends BaseActivity {
     @Override
     public void onError(int type, Object obj) {
 
+    }
+
+    public void showNoMessage(){
+        mLlNoMessage.setVisibility(View.VISIBLE);
+        mRlMyBusinessApply.setVisibility(View.GONE);
+        mRlMyBusinessFirst.setVisibility(View.GONE);
+        mRlMyBusinessReview.setVisibility(View.GONE);
+        mRlMyBusinessFinish.setVisibility(View.GONE);
+    }
+
+    /**
+     * 展示申请
+     */
+    public void showApplyMessage(){
+        mLlNoMessage.setVisibility(View.GONE);
+        mRlMyBusinessApply.setVisibility(View.VISIBLE);
+        mRlMyBusinessFirst.setVisibility(View.GONE);
+        mRlMyBusinessReview.setVisibility(View.GONE);
+        mRlMyBusinessFinish.setVisibility(View.GONE);
+    }
+
+
+    /**
+     * 展示初审
+     */
+    public void showFirstMessage(){
+        mLlNoMessage.setVisibility(View.GONE);
+        mRlMyBusinessApply.setVisibility(View.GONE);
+        mRlMyBusinessFirst.setVisibility(View.VISIBLE);
+        mRlMyBusinessReview.setVisibility(View.GONE);
+        mRlMyBusinessFinish.setVisibility(View.GONE);
+    }
+
+
+    /**
+     * 展示复审
+     */
+    public void showReviewMessage(){
+        mLlNoMessage.setVisibility(View.GONE);
+        mRlMyBusinessApply.setVisibility(View.VISIBLE);
+        mRlMyBusinessFirst.setVisibility(View.GONE);
+        mRlMyBusinessReview.setVisibility(View.GONE);
+        mRlMyBusinessFinish.setVisibility(View.GONE);
+    }
+
+    /**
+     * 展示完成
+     */
+    public void showFinishMessage(){
+        mLlNoMessage.setVisibility(View.GONE);
+        mRlMyBusinessApply.setVisibility(View.GONE);
+        mRlMyBusinessFirst.setVisibility(View.GONE);
+        mRlMyBusinessReview.setVisibility(View.GONE);
+        mRlMyBusinessFinish.setVisibility(View.VISIBLE);
     }
 }
